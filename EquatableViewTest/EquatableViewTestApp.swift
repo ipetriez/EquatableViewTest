cb9a641d8c2f62077faec8940f26a4b94d24fa09
Sergey Petrosyan <ipetriez@icloud.com>
2024-11-28 10:24:41 +0400

    Initial Commit

---
+++ EquatableViewTest/EquatableViewTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  EquatableViewTestApp.swift
+//  EquatableViewTest
+//
+//  Created by Sergey Petrosyan on 28.11.24.
+//
+
+import SwiftUI
+
+@main
+struct EquatableViewTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ EquatableViewTestTests/EquatableViewTestTests.swift
@@ -0,0 +1,17 @@
+//
+//  EquatableViewTestTests.swift
+//  EquatableViewTestTests
+//
+//  Created by Sergey Petrosyan on 28.11.24.
+//
+
+import Testing
+@testable import EquatableViewTest
+
+struct EquatableViewTestTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
